b0eefbeac49745961b34d733e2219d0fdac75286
Pavlo Malyshkin <pmalyshkin@gmail.com>
2020-03-19 02:52:17 +0200

    Part1, courseinfo, step2

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import './index.css';
+
+const App = () => {
+    const course = 'Half Stack application development'
+    const parts = {
+        part1: {
+            title: 'Fundamentals of React',
+            exercises: 10,
+            link: 'https://fullstackopen.com/en/part1',
+        },
+        part2: {
+            title: 'Using props to pass data',
+            exercises: 7,
+            link: 'https://fullstackopen.com/en/part2',
+        },
+        part3: {
+            title: 'State of a component',
+            exercises: 14,
+            link: 'https://fullstackopen.com/en/part3',
+        },
+    }
+  
+    return (
+        <div className="app-container">
+            <Header course={course} />
+            <Content parts={parts}/>
+            <Total parts={parts}/>
+        </div>
+    )
+}
+
+const Header = (props) => {
+    return (
+        <h1 className="l-tac">{props.course}</h1>
+    );
+};
+
+const Content = (props) => {
+    const parts = [];
+    for (let part in props.parts) {
+        parts.push(
+            <Part data={props.parts[part]} key={part}/>
+        );
+    }
+
+    return (
+        <ol className="b-parts">
+            {parts}
+        </ol>
+    )
+}
+
+const Part = (props) => {
+    return (
+        <li>
+            <a href={props.data.link}>{props.data.title}:</a> {props.data.exercises}
+        </li>
+    )
+}
+
+const Total = (props) => {
+    const total = Object.values(props.parts).reduce((total, {exercises}) => total + exercises, 0);
+
+    return (
+        <p className="b-total">
+            <strong>Number of exercises:</strong> {total}
+        </p>
+    )
+}
+
+export default App;
